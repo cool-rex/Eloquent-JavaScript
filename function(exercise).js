03284814122ace9b7cf529d9567aedd68db5a419
cool-rex <rbrefo2@gmail.com>
2023-09-08 13:05:58 +0000

    update

--- function(exercise).js
+++ function(exercise).js
@@ -1,10 +1,36 @@
 //Minimum
 
-function minimum(a,b) {
+/*function minimum(a,b) {
     if (a < b){
         return a;
     }else{
         return b;
     }
 }
-console.log(minimum(2,1));
+console.log(minimum(2,1));*/
+
+
+// Recursion
+
+function isEven(n){
+    if(n%2 ==0){
+        return true
+    }
+    else if( n % 2 != 0){
+        return false
+    }
+    else if(n < 0){
+        return isEven(n-2)
+    }
+    else{
+        return isEven(n-2)
+    }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+
+
+
