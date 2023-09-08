dabd6793963c009ba6263ecc03f6089f45d81173
cool-rex <rbrefo2@gmail.com>
2023-09-08 14:56:29 +0000

    update

--- function(exercise).js
+++ function(exercise).js
@@ -1,13 +1,13 @@
 //Minimum
 
-/*function minimum(a,b) {
+function minimum(a,b) {
     if (a < b){
         return a;
     }else{
         return b;
     }
 }
-console.log(minimum(2,1));*/
+console.log(minimum(2,1));
 
 
 // Recursion
@@ -31,6 +31,21 @@ console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
 
+function countChar(string, char) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == char) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBs(string) {
+    return countChar(string, "B");
+  }
+  
+  console.log(countBs("Bakery"));
 
-
-
+  console.log(countChar("bike", "i"));
+  
