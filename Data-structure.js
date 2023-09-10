ee170feb31bb08906b6a5abfab6a87f2763b6593
cool-rex <rbrefo2@gmail.com>
2023-09-10 22:35:08 +0000

    update

--- Data-structure.js
+++ Data-structure.js
@@ -1,7 +1,19 @@
 // Array
 
-let listOfNumbers = [2, 3, 5, 7, 11];
-console.log(listOfNumbers[2]);
+// let listOfNumbers = [2, 3, 5, 7, 11];
+// console.log(listOfNumbers[2]);
 
-console.log(listOfNumbers[0]);
-console.log(listOfNumbers[2 - 1]);
+// console.log(listOfNumbers[0]);
+// console.log(listOfNumbers[2 - 1]);
+
+// Method
+// let doh = "Doh";
+// console.log(doh.toUpperCase());
+
+// let sequence = [1, 2, 3];
+// sequence.push(4);
+// sequence.push(5);
+// console.log(sequence);
+// sequence.pop();
+
+// console.log(sequence)
