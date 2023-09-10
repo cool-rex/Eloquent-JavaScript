c63962695b4cc41b8a7dcdef81cb4f45230438f7
cool-rex <rbrefo2@gmail.com>
2023-09-10 22:54:47 +0000

    update

--- Data-structure.js
+++ Data-structure.js
@@ -1,19 +1,29 @@
-Array
+// Array
 
-let listOfNumbers = [2, 3, 5, 7, 11];
-console.log(listOfNumbers[2]);
+// let listOfNumbers = [2, 3, 5, 7, 11];
+// console.log(listOfNumbers[2]);
 
-console.log(listOfNumbers[0]);
-console.log(listOfNumbers[2 - 1]);
+// console.log(listOfNumbers[0]);
+// console.log(listOfNumbers[2 - 1]);
 
-Method
-let doh = "Doh";
-console.log(doh.toUpperCase());
+// Method
+// let doh = "Doh";
+// console.log(doh.toUpperCase());
 
-let sequence = [1, 2, 3];
-sequence.push(4);
-sequence.push(5);
-console.log(sequence);
-sequence.pop();
+// let sequence = [1, 2, 3];
+// sequence.push(4);
+// sequence.push(5);
+// console.log(sequence);
+// sequence.pop();
 
-console.log(sequence)
+// console.log(sequence)
+
+// Object
+let day1={
+    squirrel: false,
+    activities: ['exercise', 'read bible', 'took a long nap', 'run errand', 'eat', 'study']
+};
+
+console.log(day1.wolf)
+
+
